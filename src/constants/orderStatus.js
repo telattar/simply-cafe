2dd9f75c7d65560a8a86846ee0e008723c38c737
telattar <tarteelafattahibrahim@gmail.com>
2024-06-21 12:33:46 +0300

    add order status

---
+++ src/constants/orderStatus.js
@@ -0,0 +1,9 @@
+export const PREPARING = "Being Prepared";
+const COMPLETE = "Complete";
+const CANCELLED = "Cancelled";
+
+export const orderStatus = [
+    PREPARING,
+    COMPLETE,
+    CANCELLED
+];
